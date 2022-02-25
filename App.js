d07850bd1ce69f9e40b40f8195fc9840c672f95d
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-02-25 04:10:01 +0100

    5.1 creating start + chat

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Start from './components/Start';
+import Chat from './components/Chat';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Screen1"
+        >
+          <Stack.Screen
+            name="Screen1"
+            component={Screen}
+          />
+          <Stack.Screen
+            name="Screen2"
+            component={Screen2}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
